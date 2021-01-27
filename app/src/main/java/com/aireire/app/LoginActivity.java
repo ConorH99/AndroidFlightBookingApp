5a77fef9c21bf3d34c4785402a22204fd672a6c6
ConorH99 <conor.heeney56@gmail.com>
2021-01-27 01:09:50 +0000

    Modified validation to make code more maintainable and added code to enter data into database if no field is empty

--- app/src/main/java/com/aireire/app/LoginActivity.java
+++ app/src/main/java/com/aireire/app/LoginActivity.java
@@ -6,15 +6,17 @@ import androidx.appcompat.widget.Toolbar;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
-import android.widget.TextView;
 
-import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 
 public class LoginActivity extends AppCompatActivity {
 
     UserDatabase db;
     UserDao userDao;
+    TextInputLayout emailView = findViewById(R.id.email_entry_view);
+    TextInputLayout passwordView = findViewById(R.id.password_entry_view);
+    String emailText = emailView.getEditText().getText().toString();
+    String passwordText = passwordView.getEditText().getText().toString();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,19 +30,26 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     public void loginOnClick(View view) {
-        TextInputLayout emailView = findViewById(R.id.email_entry_view);
-        checkIfEmpty(emailView);
-        TextInputLayout passwordView = findViewById(R.id.password_entry_view);
-        checkIfEmpty(passwordView);
+        System.out.println(areAnyFieldsEmpty());
+        //TODO Log in functionality
     }
 
-    private void checkIfEmpty(TextInputLayout textBox) {
+    private boolean isFieldEmpty(TextInputLayout textBox) {
         if (textBox.getError() != null) {
             textBox.setError(null);
         }
         String enteredText = textBox.getEditText().getText().toString();
         if (TextUtils.isEmpty(enteredText)) {
             textBox.setError(getString(R.string.required_text));
+            return true;
         }
+        return false;
+    }
+
+    private boolean areAnyFieldsEmpty() {
+        if (!(isFieldEmpty(emailView) || isFieldEmpty(passwordView))) {
+            return false;
+        }
+        return true;
     }
 }
--- app/src/main/java/com/aireire/app/SignupActivity.java
+++ app/src/main/java/com/aireire/app/SignupActivity.java
@@ -6,15 +6,21 @@ import androidx.appcompat.widget.Toolbar;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
-import android.widget.TextView;
 
-import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
 
 public class SignupActivity extends AppCompatActivity {
 
     UserDatabase db;
     UserDao userDao;
+    TextInputLayout firstNameView = findViewById(R.id.first_name_entry_view);
+    TextInputLayout lastNameView = findViewById(R.id.last_name_entry_view);
+    TextInputLayout emailView = findViewById(R.id.email_entry_view);
+    TextInputLayout passwordView = findViewById(R.id.password_entry_view);
+    String firstNameText = firstNameView.getEditText().getText().toString();
+    String lastNameText = lastNameView.getEditText().getText().toString();
+    String emailText = emailView.getEditText().getText().toString();
+    String passwordText = passwordView.getEditText().getText().toString();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,23 +34,30 @@ public class SignupActivity extends AppCompatActivity {
     }
 
     public void signupOnClick(View view) {
-        TextInputLayout firstNameView = findViewById(R.id.first_name_entry_view);
-        checkIfEmpty(firstNameView);
-        TextInputLayout lastNameText = findViewById(R.id.last_name_entry_view);
-        checkIfEmpty(lastNameText);
-        TextInputLayout emailView = findViewById(R.id.email_entry_view);
-        checkIfEmpty(emailView);
-        TextInputLayout passwordView = findViewById(R.id.password_entry_view);
-        checkIfEmpty(passwordView);
+
+        if (!(areAnyFieldsEmpty())) {
+            User user = new User(firstNameText, lastNameText, emailText, passwordText);
+            userDao.insertUser(user);
+        }
     }
 
-    private void checkIfEmpty(TextInputLayout textBox) {
+    private boolean isFieldEmpty(TextInputLayout textBox) {
         if (textBox.getError() != null) {
             textBox.setError(null);
         }
         String enteredText = textBox.getEditText().getText().toString();
         if (TextUtils.isEmpty(enteredText)) {
             textBox.setError(getString(R.string.required_text));
+            return true;
         }
+        return false;
+    }
+
+    private boolean areAnyFieldsEmpty() {
+
+        if (!(isFieldEmpty(firstNameView) || isFieldEmpty(lastNameView) || isFieldEmpty(emailView) || isFieldEmpty(passwordView))) {
+            return false;
+        }
+        return true;
     }
 }
